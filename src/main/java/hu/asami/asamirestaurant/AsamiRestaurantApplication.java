59007ed2043ed176549d87324d0e12921e1510f0
robert.jantyik <robert.jantyik@dextraline.hu>
2022-08-31 16:18:26 +0200

    component scan

--- src/main/java/hu/asami/asamirestaurant/AsamiRestaurantApplication.java
+++ src/main/java/hu/asami/asamirestaurant/AsamiRestaurantApplication.java
@@ -2,8 +2,10 @@ package hu.asami.asamirestaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "hu.asami" })
 public class AsamiRestaurantApplication {
 
     public static void main(String[] args) {
