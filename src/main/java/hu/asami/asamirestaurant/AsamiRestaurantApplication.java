16dd717163220e5e80adf54ea1edcd89d1dc1805
robert.jantyik <robert.jantyik@dextraline.hu>
2022-09-16 11:37:16 +0200

    notif service

--- src/main/java/hu/asami/asamirestaurant/AsamiRestaurantApplication.java
+++ src/main/java/hu/asami/asamirestaurant/AsamiRestaurantApplication.java
@@ -3,6 +3,7 @@ package hu.asami.asamirestaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "hu.asami" })
